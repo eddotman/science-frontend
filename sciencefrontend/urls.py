097602d514a5f62aee2e9ee4fd2c699da2d7355c
Edward Kim <eddotman@gmail.com>
2013-04-30 13:57:07 -0400

    css

--- sciencefrontend/urls.py
+++ sciencefrontend/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from sciencefrontend.views import * 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
