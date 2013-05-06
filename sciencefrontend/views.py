13b497e0cf76c22449a5f377307e98bb4fabfc13
Edward Kim <eddotman@gmail.com>
2013-05-06 23:41:49 +0100

    numpy

--- sciencefrontend/views.py
+++ sciencefrontend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from numpy import *
 import datetime
 
 def test_output(request):
 	now = datetime.datetime.now()
-	return render(request, 'home.html', {'test_output':now})
+	test_numpy = cos(pi)
+	return render(request, 'home.html', {'test_output':now, 'numpy_output': test_numpy})
