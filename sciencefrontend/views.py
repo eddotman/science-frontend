731e808f1b29329d0b527af9fc3b312965c4c85f
Edward Kim <eddotman@gmail.com>
2013-05-23 09:29:20 -0400

    updated link

--- sciencefrontend/views.py
+++ sciencefrontend/views.py
@@ -5,7 +5,7 @@ def home(request):
 	from scripts.home_content import echo_home
 
 	script_name = "home_content.py"
-	script_link = "http://www.github.com/eddotman/"
+	script_link = "https://github.com/eddotman/science-frontend/blob/master/sciencefrontend/scripts/home_content.py"
 	script_gui = echo_home()
 	
 	return render(request, 'home.html', {'script_name': script_name, 'script_link': script_link, 'script_gui': script_gui})
