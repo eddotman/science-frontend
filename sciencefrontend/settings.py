715517aecb0fd91dccf1a5fca2bb9eea6d5a0846
Eddie Kim <eddotman@gmail.com>
2014-04-22 18:29:51 -0400

    trying to fix

--- sciencefrontend/settings.py
+++ sciencefrontend/settings.py
@@ -2,7 +2,7 @@
 import os
 import psycopg2
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
